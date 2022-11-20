bc3f085da995e967d03afbb383487a401b234fd5
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2022-11-21 00:34:40 +0100

    Update Book.hpp
    Da verificare se effettivamente valido una volta corretto l'operator<<

--- Headers/Book.hpp
+++ Headers/Book.hpp
@@ -18,8 +18,8 @@ public:
     /*Costruttore di default della classe Book che sfrutta la init list
     NOTA: non e' necessario il controllo degli argomenti del costruttore, gia' effettuato all'interno delle classi
     Isnb e Date; ogni Book creato e' reso disponibile*/
-    Book() : nome{""}, cognome{""}, titolo{""}, isbn{Isbn()}, data{Date()} {}
-    Book(std::string, std::string, std::string, std::string, Date d = Date());
+    Book() : nome{""}, cognome{""}, titolo{""}, isbn{Isbn()}/*, data{Date()}*/ {}
+    Book(std::string, std::string, std::string, std::string/*, Date d = Date()*/);
     class Invalid{};
 
     Isbn getIsbn();
@@ -34,14 +34,18 @@ public:
     void setCognome(std::string);
     void setData(Date);
     
-    bool isDisponibile(); /*gestisce lo stato del libro: */
+    bool isDisponibile();/*gestisce lo stato del libro: */
     void presta();
     void restituisci();
     
-    bool operator==(Book); //confronta due libri basandosi sul codice ISBN
-    bool operator!=(Book);
+    //bool operator==(Book); //confronta due libri basandosi sul codice ISBN
+    //bool operator!=(Book);
     std::ostream& operator<<(std::ostream&); //stampa titolo, autore, ISBN e la data di copyright su righe separate in output
+    bool operator==(Isbn&);
+    bool operator!=(Isbn&);
 
 };
+
+
 
 #endif
