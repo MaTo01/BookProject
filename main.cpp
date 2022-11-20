d4e1ca7fb6e04550890528ddfcaf2c0b6caf5b2c
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2022-11-21 00:37:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,9 @@ int main()
 {
 	std::vector<Book> shelf(10);
 	Book my_favourite_book("David", "Foster Wallace", "Una cosa divertente che non faro' mai piu'", "887-521-837-4");
+	
+	//TODO: Verifica cosa non va bene nell'operator<< (error: no match for "operator<<" (operand types are std::ostream and Isbn))
+	//std::cout<<i;
+	//std::cout<<my_favourite_book;
 	return 0;
 }
