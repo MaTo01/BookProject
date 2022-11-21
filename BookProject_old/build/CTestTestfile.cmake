03d527f0386134bcd4eee1137c818d52ff227f25
Van Sang Ho <vansang.hocorp@gmail.com>
2022-11-21 19:58:48 +0100

    spostamento in nuova cartella

---
+++ BookProject_old/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/veroc/Desktop/ProgettoBook-20221116T152321Z-001/ProgettoBook
+# Build directory: C:/Users/veroc/Desktop/ProgettoBook-20221116T152321Z-001/ProgettoBook/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/veroc/Desktop/ProgettoBook-20221116T152321Z-001/ProgettoBook
-# Build directory: C:/Users/veroc/Desktop/ProgettoBook-20221116T152321Z-001/ProgettoBook/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
