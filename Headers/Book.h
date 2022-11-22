15c75bc47d702e330d0d41473a25434a48ecf952
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2022-11-22 18:32:30 +0100

    Update Book.h

--- Headers/Book.h
+++ Headers/Book.h
@@ -24,7 +24,6 @@ public:
      *            costruttore di default
      *
      * - cinque argomenti: si inizializza un libro con titolo, autore, isbn e data di copyright.
-     *
      */
     Book() : nome{""}, cognome{""}, titolo{""}, isbn{Isbn()}, data{Date()} {}
     Book(const std::string& n, const std::string& c, const std::string& t, const std::string& i, const Date& d = Date());
@@ -33,13 +32,12 @@ public:
      *
      * Accessors / get functions: Utilizziamo l'indentazione di tipo get<var name> 
      * 
-     * Mutators / set functions: Compe per le get functions, scriviamo set<var name>
+     * Mutators / set functions: Come per le get functions, scriviamo set<var name>
      * 
      * NOTA: Definiamo le get functions all'interno della classe, dato che sono funzioni inline
      * 
      * NOTA: Definiamo isDisponibile in maniera diversa dalle get functions per evidenziare l'utilità
      *       della funzione stessa, presta() e restituisci() modificano indirettamente isDisponibile()
-     * 
      */
     Isbn getIsbn(void) const {return isbn;}
     std::string getTitolo(void) const {return titolo;}
@@ -58,19 +56,16 @@ public:
     void presta();
     void restituisci();
     
-    /*
-     * Classe definita per la gestione delle eccezioni. 
-     */
+    /* Classe definita per la gestione delle eccezioni */
     class Invalid{};
 };
 
 /* Operatori di confronto 
  * 
- * NOTA: abbiamo deciso di sfruttare al meglio le get function della classe
+ * NOTA: abbiamo deciso di sfruttare al meglio le get functions della classe
  *       Book e far confrontare direttamente due libri anzichè due singoli isbn, 
  *       anche per una questione di generalizzazione, così possiamo confrontare
- *       non solo gli isbn ma anche tutti gli altri parametri dei libri
- *
+ *       non solo gli isbn ma, eventualmente, anche tutti gli altri parametri dei libri
  */
 bool operator==(const Book& book1, const Book& book2);
 bool operator!=(const Book& book1, const Book& book2);
